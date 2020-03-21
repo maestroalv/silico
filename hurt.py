bfffb1b7a843a2860c6df5008bc29720b9c9b4bb
Autis People <60252742+LeON101-coder@users.noreply.github.com>
2020-03-21 16:35:52 +0700

    Main

--- hurt.py
+++ hurt.py
@@ -1,1 +1,1 @@
-print("\033[1;91m[!] \033[1;92mTunggu Rilis Resmi nya\nJangan lupa Follow untuk mendapatkan\n   pemberitahuan rilis resmi/update!")
+print("\033[1;91m[!] \033[1;92mTunggu Rilis Resmi nya\n    Jangan lupa Follow untuk mendapatkan\n    pemberitahuan rilis resmi/update!")
