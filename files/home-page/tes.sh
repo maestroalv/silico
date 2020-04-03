00e5c4ced3a9a9fc5d8c030c9f280f7ed3649a95
Autis People <60252742+LeON101-coder@users.noreply.github.com>
2020-04-04 00:11:43 +0700

    Add files via upload

---
+++ files/home-page/tes.sh
@@ -0,0 +1,11 @@
+termux-setup-storage
+cd
+cd /data/data/com.termux/files/usr/etc
+rm bash.bashrc
+cd $HOME
+cd Hurt/files/home-page
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+cd $HOME
+cd Hurt/files/home-page
+rm bash.bashrc
+cd $HOME
