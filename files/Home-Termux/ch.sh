9bf9c8cc1cd9a45b9597360a0def85a918615191
Autis People <60252742+LeON101-coder@users.noreply.github.com>
2020-04-28 10:55:56 +0700

    Add files via upload

---
+++ files/Home-Termux/ch.sh
@@ -0,0 +1,11 @@
+termux-setup-storage
+cd
+cd /data/data/com.termux/files/usr/etc
+rm bash.bashrc
+cd $HOME
+cd Hurt/files/Home-Termux
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+cd $HOME
+cd Hurt/files/Home-Termux
+rm bash.bashrc
+cd $HOME
